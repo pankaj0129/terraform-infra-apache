9f53b7d2d8e95692dff163383ece4c1fcb08960b
pankaj0129 <133801920+pankaj0129@users.noreply.github.com>
2023-10-12 16:54:56 +0530

    Update backend.tf

--- apache/backend.tf
+++ apache/backend.tf
@@ -1,8 +1,8 @@
-#terraform {
- # backend "s3" {
-  #  bucket  = "pankaj-apache"
-   # key     = "Ansible_key/terraform.tfstate"
-    #region  = "ap-south-1"
-    #encrypt = true
-  #}
-#}
+terraform {
+  backend "s3" {
+    bucket  = "pankaj-apache"
+    key     = "Ansible_key/terraform.tfstate"
+    region  = "ap-south-1"
+    encrypt = true
+  }
+}
